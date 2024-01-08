a6b6110d45321d93000c25cffff2b657595f6bf8
DongT2LS <vanhein445566@gmail.com>
2024-01-07 19:55:28 -0800

    update login authen

--- controller/usercontroller.cpp
+++ controller/usercontroller.cpp
@@ -35,43 +35,49 @@ struct Response signup(struct Request *request)
 struct Response login(struct Request *request)
 {
     struct Response response;
+    request->message[strlen(request->message) - 1] == '\n' ? request->message[strlen(request->message) - 1] = '\0' : 0;
+    cout << request->message << endl;
     response.type = RESPONSE_LOGIN;
     char *username, *password;
     username = strtok(request->message, " ");
     password = strtok(NULL, "\0");
-    // User::login(username,password,request->client_socket);
     for (User *user : users)
     {
         if (strcmp(username, user->getUsername()) == 0)
         {
+            cout << "Pass :" << password << "Correct pass : " << user->getPassword() << endl;
             if (strcmp(password, user->getPassword()) != 0)
             {
                 response.status = ERROR;
                 strcpy(response.message, "Wrong password");
-            }
-            if (user->status == USER_OFFLINE)
-            {
-                // Update client socket va trang thai cua nguoi dung
-                user->setClientSocket(request->client_socket);
-                user->status = USER_ONLINE;
-                printf("%s online !\n", user->getUsername());
-
-                // Thong bao den tat ca user cap nhat nguoi dung online va lay nhung game dang ready
-                update_list_online_user();
-                sleep(1);
-                get_ready_game(request->client_socket);
-                sleep(1);
-                // Gui response
-                char id_string[5];
-                sprintf(id_string, "%d", user->getId());
-                strcpy(response.message, id_string);
-                cout << response.message << endl;
-                response.status = SUCCESS;
                 return response;
             }
-            strcpy(response.message, "User logged !\n");
-            response.status = ERROR;
-            return response;
+            else
+            {
+                if (user->status == USER_OFFLINE)
+                {
+                    // Update client socket va trang thai cua nguoi dung
+                    user->setClientSocket(request->client_socket);
+                    user->status = USER_ONLINE;
+                    printf("%s online !\n", user->getUsername());
+
+                    // Thong bao den tat ca user cap nhat nguoi dung online va lay nhung game dang ready
+                    update_list_online_user();
+                    sleep(1);
+                    get_ready_game(request->client_socket);
+                    sleep(1);
+                    // Gui response
+                    char id_string[5];
+                    sprintf(id_string, "%d", user->getId());
+                    strcpy(response.message, id_string);
+                    cout << response.message << endl;
+                    response.status = SUCCESS;
+                    return response;
+                }
+                strcpy(response.message, "User logged !\n");
+                response.status = ERROR;
+                return response;
+            }
         }
     }
     strcpy(response.message, "Account not exist !\n");
